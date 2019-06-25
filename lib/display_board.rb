fdde4e57ccc22fa8f8cb2a42015a61faddbe6ad8
Zachary Dillon Smith <zdsmith001@gmail.com>
2019-06-25 16:10:08 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 board = ["   ","   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 
-def display_board(board[])
+def display_board(board)
   puts "#{board[0]}|#{board[1]}|#{board[2]}"
   puts "-----------"
   puts "#{board[3]}|#{board[4]}|#{board[5]}"
